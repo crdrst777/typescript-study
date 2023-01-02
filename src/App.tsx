320a8cbd7268b5032f36680fc5ca07057b9f96d3
HYEJINLEE <crdrst777@gmail.com>
2023-01-02 21:16:54 +0900

    feat: todos state추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
+import { useState } from "react";
 
 function App() {
   // 배열 안에서 Todo 클래스를 사용해 객체를 만들거임.
-  const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+  // const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+  // 위처럼 하지않고 state를 사용할거임.
+  // useState() 함수는 원래 제네릭 함수임. 우리가 이 state에 정말로 저장하려고 하는 데이터의 타입을 지정할 수 있도록 함.
+  // (처음엔 빈배열이지만) 추가될 항목의 형태는 우리가 만든 Todo 클래스의 형태여야 한다는 정의임. (이 state를 통해 관리할 데이터의 타입은 Todo로 구성된 배열이라고 정의)
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   // todoText에 값을 넘겼으니, 이제 아래같은 형태가 되는거임.
   // todos = [
@@ -17,8 +23,15 @@ function App() {
   //   },
   // ];
 
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText); // new Todo()를 사용하고, 인수로는 todoText를 넣어 생성자에게 전달.
+
+    setTodos((prevTodos) => prevTodos.concat(newTodo)); // Concat()을 사용하면 새로운 배열 생성.
+  };
+
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,13 +1,17 @@
 // 사용자에게 입력창을 제공하고 입력받은 내용을 가져오는 컴포넌트
-import { useRef } from "react";
+import React, { useRef } from "react";
 
-const NewTodo = () => {
+// '() =>' 함수 타입. 괄호 안에는 인수 즉 매개변수를, 화살표 다음에는 반환 타입을 정의한다.
+// onAddTodo: (text: string) => void
+// ----> onAddTodo함수를 props로 받아올건데, 이 함수 타입의 매개변수 타입은 문자열(매개변수 이름은 아무거나해도됨), 반환값은 없음
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   const todoTextInputRef = useRef<HTMLInputElement>(null);
   // React.FormEvent -> 폼 제출 이벤트 타입. 리액트 패키지에서 제공하는 타입.
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
 
     const enteredText = todoTextInputRef.current!.value;
+    // ref에는 항상 current 프로퍼티가 있고 여기에 실제 값이 들어있음
     // current 뒤에 ?를 쓴다면 그 이유는 타입스크립트에게 일단 값에 접근해보고, 접근이 가능하다면, 그때 입력된 값을 가져와 enteredText 저장하라고 알리는 것임.
     // 만약 접근이 불가능하면 아직 연결되지 않은 상태일 거고 null이 enteredText에 저장될거임.
     // current 뒤에 !를 쓰는것은 이 시점에는 값이 null이 아니라는, useRef와 요소가 연결되었다는게 확실하다는 것임.
@@ -18,7 +22,8 @@ const NewTodo = () => {
       // throw an error
       return;
     }
-    // 입력된 내용을 todos 목록에 추가하기.
+    // 입력된 내용을 todos 목록에 추가하기. App.tsx에서 함수를 가져올거임.
+    props.onAddTodo(enteredText);
   };
 
   return (
